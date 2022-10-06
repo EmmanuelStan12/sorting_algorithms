de189ba42de8ee86c264c9346c6d1c0cc57ddb5c
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-10-06 13:25:56 +0100

    task: Selection sort
    implemented Selection sort algorithm

---
+++ 2-selection_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array with selection sort
+ * @array: the array to sort
+ * @size: the size of the array
+ * Return: void
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min;
+
+	for (i = 0; i < size; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (compare(array[min], array[j]) == 1)
+			{
+				min = j;
+			}
+		}
+		if (min != i)
+		{
+			swap(i, min, array);
+			print_array(array, size);
+		}
+	}
+}
